4f83bb592d0346de83d5a098fc0ea53784d9d6b4
Jamel Toms <jameltoms@gmail.com>
2019-06-09 10:58:44 -0400

    Refactor to class

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,36 @@
-import { greenhouseConsumer } from './greenhouse-consumer'
+import { GreenhouseConsumer, greenhouseConsumer } from './greenhouse-consumer'
 import { greenhouseOptions, GreenhouseOptions } from './greenhouse-options'
 
-export default function(api: any, userOptions: GreenhouseOptions) {
+export default class GreenhouseSource {
+  options: GreenhouseOptions
+  consumer: GreenhouseConsumer
 
-  console.log('**** ACTIVATED ****')
+  constructor(private api: any, userOptions: Partial<GreenhouseOptions>) {
+    console.log('**** ACTIVATED ****')
+    this.options = greenhouseOptions(userOptions)
+    this.consumer = greenhouseConsumer(this.options)
+    this.source()
+  }
 
-  const options = greenhouseOptions(userOptions)
-  const consumer = greenhouseConsumer(options)
+  source() {
+    this.api.loadSource(async (store: any) => {
+      const greenhouseJobs = store.addContentType({
+        typeName: 'GreenhouseJobs',
+        route: '/job/:id'
+      })
 
-  api.loadSource(async (store: any) => {
-    const greenhouseJobs = store.addContentType({
-      typeName: 'GreenhouseJobs',
-      route: '/job/:id'
+      const greenhouseJobDetails = store.addContentType({
+        typeName: 'GreenhouseJobDetails',
+        route: '/jobDetail/:id'
+      })
+
+      const { jobs } = await this.consumer.listJobs()
+      jobs.forEach(greenhouseJobs.addNode)
+
+      jobs.forEach(async (job) => {
+        const jobDetail = await this.consumer.retrieveJob(job.id)
+        greenhouseJobDetails.addNode(jobDetail)
+      })
     })
-
-    const greenhouseJobDetails = store.addContentType({
-      typeName: 'GreenhouseJobDetails',
-      route: '/jobDetail/:id'
-    })
-
-    const { jobs } = await consumer.listJobs()
-    jobs.forEach(greenhouseJobs.addNode)
-
-    jobs.forEach(async (job) => {
-      const jobDetail = await consumer.retrieveJob(job.id)
-      greenhouseJobDetails.addNode(jobDetail)
-    })
-  })
+  }
 }
